5f1cbcc2f24d09e1b2aa7f2a6b5599fcff522c0c
xhywebdev <143572252+xhywebdev@users.noreply.github.com>
2023-10-01 22:42:59 -0700

    init project

---
+++ js/site_scripts.js
@@ -0,0 +1,17 @@
+function partners() {
+    var partners = ['bustour', 'cabinrental', 'campingadv', 'collegetours', 'rentalbike', 'tourgroup']
+    for (partner of partners) {
+        var img = `<li class="partner"><img src="images/partners/partner-${partner}.png"></li>`;
+        document.getElementById("partners").innerHTML += img;
+    }
+}
+
+partners();
+
+$(document).ready(function () {
+    $('td').click(
+        function () {
+            $(this).css({ "background-color": "red" });
+        }
+    )
+});
